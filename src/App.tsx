274c612d26057cf12d7ccb2d04f4ba00501bab66
Gabriel Barreto <barreto-gabriel@outlook.com>
2023-01-23 01:39:41 -0300

    refactor: renders form

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import * as S from './App.styled'
-import { Button, Input, Label } from './components'
+import { Form } from './components'
 import { globalStyle } from './styles'
 import { standardTheme } from './theme'
 
@@ -7,9 +7,10 @@ export function App() {
   globalStyle()
   return (
     <S.App className={standardTheme}>
-      <Label content="Poe o elenco aqui:" fieldName="cast" />
-      <Input fieldName="cast" />
-      <Button label="Sortear" onClick={() => {}} />
+      <Form
+        name="cast-shuffle"
+        field={{ name: 'cast', label: 'Qual vai ser o elenco?' }}
+      />
     </S.App>
   )
 }
