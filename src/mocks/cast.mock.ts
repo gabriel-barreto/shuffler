c4d29d43c885f34636909d5a713ee8f90a96bbab
Gabriel Barreto <barreto-gabriel@outlook.com>
2023-01-24 00:08:14 -0300

    refactor: add the member index to mock

--- src/mocks/cast.mock.ts
+++ src/mocks/cast.mock.ts
@@ -2,8 +2,10 @@ import faker from 'faker'
 
 export function makeCast({ hasHeader = false, length = 12 } = {}) {
   const header = `${faker.lorem.words()} - 25/01`
-  const data = Array.from({ length }, faker.name.firstName)
-  const raw = data.map((e, i) => `${i + 1} - ${e}`).join('\n')
+  const data = Array.from({ length }, (_, i) =>
+    [i.toString().padStart(2, '0'), faker.name.firstName()].join(' - ')
+  )
+  const raw = data.join('\n')
   if (hasHeader) return { data, raw: `${header}\n${raw}` }
   return { data, raw }
 }
