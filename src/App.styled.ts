ea2dc44445b0c467dc980326f1067c805c1f1c6b
Gabriel Barreto <barreto-gabriel@outlook.com>
2023-01-24 00:08:42 -0300

    feat: consume shuffler service, render chunks

--- src/App.styled.ts
+++ src/App.styled.ts
@@ -3,5 +3,6 @@ import { styled } from '@/theme'
 export const App = styled('main', {
   backgroundColor: '$gray900',
   height: '100vh',
+  overflowY: 'scroll',
   padding: '$2'
 })
--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,35 @@
+import { useRef, useState } from 'react'
 import * as S from './App.styled'
-import { Form } from './components'
+import { Chunk, Form } from './components'
+import { shuffle } from './service'
 import { globalStyle } from './styles'
 import { standardTheme } from './theme'
 
 export function App() {
+  const scrollableContainer = useRef<HTMLElement>(null)
+  const [chunks, setChunks] = useState<string[][]>([])
   globalStyle()
+
+  function scrollDown() {
+    if (scrollableContainer.current === null) return
+    const [{ height }] = scrollableContainer.current.getClientRects()
+    setTimeout(() => {
+      scrollableContainer.current?.scrollTo({ behavior: 'smooth', top: height })
+    }, 100)
+  }
+
+  function onSubmit(cast: string[]) {
+    const chunks = shuffle(cast)
+    setChunks(chunks)
+    scrollDown()
+  }
+
   return (
-    <S.App className={standardTheme}>
-      <Form onSubmit={console.log} />
+    <S.App className={standardTheme} ref={scrollableContainer}>
+      <Form onSubmit={onSubmit} />
+      {chunks.map(members => (
+        <Chunk key={members[0]} members={members} />
+      ))}
     </S.App>
   )
 }
