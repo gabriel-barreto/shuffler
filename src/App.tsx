dbee6387a97299748a414d6d9f4aaa8a8441be5c
Gabriel Barreto <barreto-gabriel@outlook.com>
2023-01-26 02:05:45 -0300

    feat: consume teams from store

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import { Team } from '@/components/teams'
-import { useRef, useState } from 'react'
+import { useRef } from 'react'
 import * as S from './App.styled'
 import { Form, Teams } from './components'
 import { shuffleTeams } from './service'
+import { useTeams } from './store'
 import { globalStyle } from './styles'
 import { standardTheme } from './theme'
 
 export function App() {
   const scrollableContainer = useRef<HTMLElement>(null)
-  const [teams, setTeams] = useState<Team[]>([])
+  const { setTeams, teams } = useTeams()
   globalStyle()
 
   function scrollDown() {
