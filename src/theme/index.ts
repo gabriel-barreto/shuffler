264c327ca4f92dd34c7a643fc302cb6b3f0a853e
Gabriel Barreto <barreto-gabriel@outlook.com>
2023-01-25 23:40:06 -0300

    feat: define breakpoints

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,5 +1,13 @@
 import { createStitches } from '@stitches/react'
 
-export const { styled } = createStitches({})
+export const { styled } = createStitches({
+  media: {
+    xs: '(min-width: 640px)',
+    sm: '(min-width: 768px)',
+    md: '(min-width: 1024px)',
+    lg: '(min-width: 1280px)',
+    xl: '(min-width: 1600px)'
+  }
+})
 
 export { standardTheme } from './standard'
